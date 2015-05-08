b2645bf4c1454a86e8826a249876657b0b78b7c8
TYakumo <foxs91092@gmail.com>
2015-05-08 22:26:27 +0800

    PA fixed

--- 2014/Round3/A.cpp
+++ 2014/Round3/A.cpp
@@ -9,14 +9,17 @@ long long P, Q, R, S;
 
 inline long long sumOfRange(int s, int t)
 {
+    if(s > t) {
+        return 0;
+    }
     long long from = s ? sum[s-1] : 0, to = sum[t];
     return to-from;
 }
 
 inline long long ArnarGet(int s, int t)
 {
-    long long inRange = sumOfRange(s, t);
-    return min(inRange, sum[N]-inRange);
+    long long Solveig = max(sumOfRange(s, t), max(sumOfRange(0, s-1), sumOfRange(t+1, N-1)));
+    return sum[N]-Solveig;
 }
 
 int main()
@@ -35,15 +38,15 @@ int main()
         }
 
         sum[N] = sum[N-1];
-        long long ans = 0; 
-        long long halfSum = sum[N]/2;
+        long long ans = 0, halfSum = sum[N]/2, thirdSum = sum[N]/3; 
         int rightBound = 0;
 
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < N; ++i) {
+
             while( rightBound < N) {
                 ans = max(ans, ArnarGet(i, rightBound));
                 ++rightBound;
-                if(rightBound < N && sumOfRange(i, rightBound) >= halfSum) {
+                if(rightBound < N && sumOfRange(i, rightBound) >= thirdSum) {
                     ans = max(ans, ArnarGet(i, rightBound));
                     rightBound = max(i+1, rightBound-1);
                     break;
@@ -51,7 +54,7 @@ int main()
             }
         }
 
-        printf("Case %d: %.12f\n", tc, (double)ans/(double)sum[N]);
+        printf("Case #%d: %.12f\n", tc, (double)ans/(double)sum[N]);
     }
     return 0;
 }
