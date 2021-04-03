1f6aff9546356a9bf6fd717ecdbc96d2cfcedf63
Lin Huan Po <foxs91092@gmail.com>
2021-04-03 22:39:58 +0900

    2019 1C - PC large up-solved

--- 2019/1C/C.cpp
+++ 2019/1C/C.cpp
@@ -10,113 +10,97 @@
 #include <unordered_map>
 using namespace std;
 
-using MSI = unordered_map<string, int>;
+using MSI = unordered_map<int, int>;
 using VS = vector <string>;
 using VI = vector <int>;
 using VVI = vector <VI>;
 const int VTYPE = 0;
 const int HTYPE = 1;
-const int CHECKMODE = 0;
-const int PUTMODE = 1;
 
-bool handler(VS& colony, int ir, int ic, int type, int mode) {
-    int R = colony.size();
-    int C = colony[0].size();
-    VS temp = colony;
-    VS &handle = mode == CHECKMODE ? temp : colony;
+inline int getAreaCnt(const VVI& precal, int sr, int er, int sc, int ec) {
+    int ret = precal[er][ec];
 
-    deque <VI> que;
-    que.push_back({ir, ic});
-    handle[ir][ic] = 'X';
-
-    while (!que.empty()) {
-        int r = que.front()[0];
-        int c = que.front()[1];
-        que.pop_front();
-        int sign = 1;
-
-        for (int d = 0; d < 2; ++d) {
-            int nr = r;
-            int nc = c;
-
-            if (type == VTYPE) {
-                nr += sign;
-            } else {
-                nc += sign;
-            }
-
-            if (nr >= 0 && nr < R
-                && nc >= 0 && nc < C) {
-                if (handle[nr][nc] == '#') {
-                    return false;
-                } else if (handle[nr][nc] == '.') {
-                    handle[nr][nc] = 'X';
-                    que.push_back({nr, nc});
-                }
-            }
-
-            sign *= -1;
-        }
+    if  (sr-1 >= 0) {
+        ret -= precal[sr-1][ec];
     }
 
-    return true;
+    if (sc-1 >= 0) {
+        ret -= precal[er][sc-1];
+    }
+
+    if (sr-1 >= 0 && sc-1 >= 0) {
+        ret += precal[sr-1][sc-1];
+    }
+
+    return ret;
 }
 
-int solve(int& ans, MSI& dp, VS colony, int turn, int init) {
-    string str;
-    // cout << "-----DEBUG------" << endl;
-    for (int i = 0; i < colony.size(); ++i) {
-        str += colony[i];
-        // cout << colony[i] << endl;
+bool checkOK(const VVI& precal, int SR, int ER, int SC, int EC, int r, int c, int type) {
+    if (type == VTYPE) {
+        return getAreaCnt(precal, SR, ER, c, c) == 0;
+    } else {
+        return getAreaCnt(precal, r, r, SC, EC) == 0;
+    }
+}
+
+int solve(int& ans, MSI& dp, const VVI& precal, int SR, int ER, int SC, int EC, int turn, int init) {
+    if (SC > EC || SR > ER) {
+        return 0;
     }
 
-    // cout << "-----DEBUG END------" << endl;
+    int state = SR*(1LL<<16) + ER*(1LL<<8) + SC*(1LL<<4) + EC;
 
-    if (dp.count(str)) {
-        return dp[str];
+    if (dp.count(state)) {
+        return dp[state];
     }
 
-    int res = 0;
-    int R = colony.size();
-    int C = colony[0].size();
+    VI child;
 
-    for (int r = 0; r < R; ++r) {
-        for (int c = 0; c < C; ++c) {
-            if (colony[r][c] == '.') {
-                // put V
-                if (handler(colony, r, c, VTYPE, CHECKMODE)) {
-                    VS newcolony = colony;
-                    handler(newcolony, r, c, VTYPE, PUTMODE);
+    int R = precal.size();
+    int C = precal[0].size();
 
-                    if (solve(ans, dp, newcolony, turn^1, 0) == 0) {
-                        if (init) {
-                            res = 1;
-                            ++ans;
-                        } else {
-                            return dp[str] = 1;
-                        }
-                    }
-                }
+    for (int cr = SR; cr <= ER; ++cr) {
+        if (checkOK(precal, SR, ER, SC, EC, cr, SC, HTYPE)) {
+            int res1 = solve(ans, dp, precal, SR, cr-1, SC, EC, turn^1, 0);
+            int res2 = solve(ans, dp, precal, cr+1, ER, SC, EC, turn^1, 0);
 
-                // put H
-                if (handler(colony, r, c, HTYPE, CHECKMODE)) {
-                    VS newcolony = colony;
-                    handler(newcolony, r, c, HTYPE, PUTMODE);
-
-                    if (solve(ans, dp, newcolony, turn^1, 0) == 0) {
-                        if (init) {
-                            res = 1;
-                            ++ans;
-                        } else {
-                            return dp[str] = 1;
-                        }
-                    }
-                }
+            if (init && (res1 ^ res2) == 0) {
+                ans += (EC-SC+1);
             }
+
+            child.push_back(res1^res2);
         }
     }
 
-    return dp[str] = res;
+    for (int cc = SC; cc <= EC; ++cc) {
+        if (checkOK(precal, SR, ER, SC, EC, SR, cc, VTYPE)) {
+            int res1 = solve(ans, dp, precal, SR, ER, SC, cc-1, turn^1, 0);
+            int res2 = solve(ans, dp, precal, SR, ER, cc+1, EC, turn^1, 0);
+
+            if (init && (res1 ^ res2) == 0) {
+                ans += (ER-SR+1);
+            }
+
+            child.push_back(res1^res2);
+        }
+    }
+
+    sort(child.begin(), child.end());
+    if (!child.empty() && child[0] != 0) {
+        return dp[state] = 0;
+    }
+
+    for (int i = 1; i < child.size(); ++i) {
+        if (child[i] > child[i-1]+1) {
+            return dp[state] = child[i-1]+1;
+        }
+    }
+
+    if (child.size()) {
+        return dp[state] = child.back()+1;
+    }
+
+    return dp[state] = 0;
 }
 
 int main()
@@ -131,14 +115,30 @@ int main()
         cin >> R >> C;
 
         VS colony(R);
+        VVI precal(R, VI(C));
+
         for (int i = 0; i < R; ++i) {
             cin >> colony[i];
         }
 
+        for (int i = 0; i < R; ++i) {
+            int acc = 0;
+            for (int j = 0; j < C; ++j) {
+                if (colony[i][j] == '#') {
+                    ++acc;
+                }
+
+                precal[i][j] = acc;
+
+                if (i) {
+                    precal[i][j] += precal[i-1][j];
+                }
+            }
+        }
+
         MSI dp;
         int ans = 0;
-        solve(ans, dp, colony, 0, 1);
-
+        solve(ans, dp, precal, 0, R-1, 0, C-1, 0, 1);
 
         cout << "Case #" << tc << ": " << ans << endl;
     }
